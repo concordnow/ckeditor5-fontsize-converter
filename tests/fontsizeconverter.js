9a6115876f52a86a56bff61d2da0c0b592092ba6
thiberaw <thibautware@outlook.com>
2019-12-11 15:42:45 +0000

    Internal: move downcast converter to a separated file

--- tests/fontsizeconverter.js
+++ tests/fontsizeconverter.js
@@ -1,10 +1,11 @@
 import FontSizeConverter from './../src/fontsizeconverter';
 import ParagraphFontSizeConverter from './../src/paragraphfontsizeconverter';
 import SpanFontSizeConverter from './../src/spanfontsizeconverter';
+import DowncastConverter from './../src/downcastconverter';
 
 describe( 'FontSizeConverter', () => {
 	it( 'requires ParagraphFontSizeConverter', () => {
-		expect( FontSizeConverter.requires ).to.deep.equal( [ ParagraphFontSizeConverter, SpanFontSizeConverter ] );
+		expect( FontSizeConverter.requires ).to.deep.equal( [ ParagraphFontSizeConverter, SpanFontSizeConverter, DowncastConverter ] );
 	} );
 
 	it( 'defines plugin name', () => {
